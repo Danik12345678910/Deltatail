5e29b0123e8a7d69d63d3d3b3ef6cc5e60a2bd38
Danik12345678910 <166648814+Danik12345678910@users.noreply.github.com>
2025-09-05 15:49:52 +0400

    Delete AudioDialogData.cs

--- Assets/Scripts/Dialog/AudioDialog/AudioDialogData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AudioDialogData", menuName = "Scriptable Objects/AudioDialogData")]
-public class AudioDialogData : ScriptableObject
-{
-    [field: SerializeField] public AudioClip[] AllSounds { get; private set; }
-    public AudioClip RandomSound { get => AllSounds[Random.Range(0, AllSounds.Length)]; }
-}
